c6d74e19d4516037c189ac4cb397c46dd01022ef
fathularif <fathulcode@gmail.com>
2022-06-22 23:46:18 +0800

    add .env

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ const cors = require("cors");
 const app = express();
 const useRoutes = require("./routes/user");
 const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
 const options = {
   origin: "http://localhost:3000",
   useSuccessStatus: 200,
 };
+
+dotenv.config();
 
 app.use(cors(options));
 app.use("/api", useRoutes);
@@ -15,6 +18,8 @@ readdirSync("./routes").map((route) => {
   app.use("/api/v1", require("./routes/" + route));
 });
 
-app.listen(8000, () => {
-  console.log("server is listening...");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
 });
