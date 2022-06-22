e842fa2332a890db6d7a7e277c246b645263f480
fathularif <fathulcode@gmail.com>
2022-06-22 22:25:05 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const options = {
+  origin: "http://localhost:3000",
+  useSuccessStatus: 200,
+};
+
+app.use(cors(options));
+
+app.get("/", (req, res) => {
+  res.send("welcome from home");
+});
+app.get("/books", (req, res) => {
+  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
+});
+app.listen(8000, () => {
+  console.log("server is lestining...");
+});
