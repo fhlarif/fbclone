fe3d6404a7cb161f3f035d8c70d4763bf7ff9a7d
fathularif <fathulcode@gmail.com>
2022-06-23 20:37:46 +0800

    add mongodb connectiom

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 const useRoutes = require("./routes/user");
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
+
+/* Enable env */
+dotenv.config();
+
+/* Routes */
+/* Setup Cors */
 const options = {
   origin: "http://localhost:3000",
   useSuccessStatus: 200,
 };
-
-dotenv.config();
 
 app.use(cors(options));
 app.use("/api", useRoutes);
@@ -18,6 +23,15 @@ readdirSync("./routes").map((route) => {
   app.use("/api/v1", require("./routes/" + route));
 });
 
+/* Database */
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error connecting on mongodb", err));
+
+/* Port */
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
